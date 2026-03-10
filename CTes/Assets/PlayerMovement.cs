9e80ef85c25953b4a207312c08fec709f2f25b32
Huynh Van Tien <tesiuem@gmail.vom>
2026-03-10 13:45:03 +0700

    commit

---
+++ CTes/Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(0, 7);
+        }
+    }
+}
+
